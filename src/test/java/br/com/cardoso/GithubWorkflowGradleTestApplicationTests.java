3c7aa77cc2765221f7afb11439e9229b2d3f30a4
cardoso-thiago <thiago.cardoso33@gmail.com>
2022-08-27 23:01:26 -0300

    Commit inicial

---
+++ src/test/java/br/com/cardoso/GithubWorkflowGradleTestApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.cardoso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubWorkflowGradleTestApplicationTests {
+
+	@Test
+	void contextLoads() throws InterruptedException {
+		Thread.sleep(30000);
+	}
+
+}
